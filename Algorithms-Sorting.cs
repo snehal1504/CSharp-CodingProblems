bacfcdc231b11df62c6b501fb16ea5d2fd5b5f73
Snehal <47214795+snehal1504@users.noreply.github.com>
2026-07-13 02:22:51 +0530

    Create Algorithms-Sorting.cs

---
+++ Algorithms-Sorting.cs
@@ -0,0 +1,27 @@
+//Sorting-insertionSort1
+
+public static void insertionSort1(int n, List<int> arr)
+    {
+    for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];      // element to insert
+            int j = i - 1;
+
+            // Shift elements greater than key to the right
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+                Console.WriteLine(string.Join(" ", arr));
+            }
+
+            // Insert key at correct position
+            arr[j + 1] = key;
+            
+            
+        }
+        Console.WriteLine(string.Join(" ", arr));
+        
+    }
+
+}
